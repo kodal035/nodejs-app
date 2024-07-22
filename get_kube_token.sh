df24ac34760bd5d44a3ba95fd0ae3adfed7b4077
Abdullah KODAL <108393640+kodal035@users.noreply.github.com>
2024-07-22 15:16:20 +0300

    Update get_kube_token.sh

--- get_kube_token.sh
+++ get_kube_token.sh
@@ -1,3 +1,7 @@
-data "external" "kubernetes_token" {
-  program = ["bash", "${path.module}/scripts/get_kube_token.sh"]
-}
+#!/bin/bash
+
+# Token'i al
+TOKEN=$(kubectl create token jenkins -n jenkins --duration=8760h)
+
+# Token'i dosyaya yaz
+echo $TOKEN > /usr/local/bin/kube_token.txt
