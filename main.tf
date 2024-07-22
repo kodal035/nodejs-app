0a47fe6f0c2884b61f04b547aa14cd01f58d6f56
Abdullah KODAL <108393640+kodal035@users.noreply.github.com>
2024-07-22 14:52:16 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,22 +48,10 @@ resource "null_resource" "install_minikube" {
 resource "null_resource" "install_jenkins" {
   provisioner "local-exec" {
     command = <<EOF
-      # Jenkins Keyring'ini İndirin ve Kurun
-      sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-        https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-
-      # Jenkins Depo Kaydını Ekleyin
-      echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-        https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
-        /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-      # Paket Listelerini Güncelleyin
+      sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+      echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
       sudo apt-get update
-
-      # Jenkins'i Kurun
       sudo apt-get install -y jenkins
-
-      # Jenkins Servisini Başlatın ve Etkinleştirin
       sudo systemctl enable jenkins
       sudo systemctl start jenkins
     EOF
@@ -89,7 +77,7 @@ resource "null_resource" "update_kube_config" {
       CA_DATA=$(cat ~/.kube/config | grep 'certificate-authority-data:' | awk '{print $2}')
       CLIENT_CERT_DATA=$(cat ~/.minikube/profiles/minikube/client.crt | base64 -w 0)
       CLIENT_KEY_DATA=$(cat ~/.minikube/profiles/minikube/client.key | base64 -w 0)
-      
+
       sed -i "s/\"certificate-authority\": \".*\"/\"certificate-authority-data\": \"${CA_DATA}\"/" /var/lib/jenkins/.kube/config
       sed -i "s/\"client-certificate\": \".*\"/\"client-certificate-data\": \"${CLIENT_CERT_DATA}\"/" /var/lib/jenkins/.kube/config
       sed -i "s/\"client-key\": \".*\"/\"client-key-data\": \"${CLIENT_KEY_DATA}\"/" /var/lib/jenkins/.kube/config
@@ -113,14 +101,10 @@ resource "null_resource" "create_kubernetes_resources" {
 resource "null_resource" "create_pipeline" {
   provisioner "local-exec" {
     command = <<EOF
-      # GitHub'dan pipeline_config.xml'i çek
       curl -L -o pipeline_config.xml https://github.com/kodal035/nodejs-app/raw/main/pipeline_config.xml
-
-      # Jenkins CLI ile pipeline oluştur
       if ! command -v jenkins-cli > /dev/null; then
         wget http://localhost:8080/jnlpJars/jenkins-cli.jar
       fi
-
       java -jar jenkins-cli.jar -s http://localhost:8080 create-job nodejs-app < pipeline_config.xml
     EOF
   }
@@ -132,9 +116,9 @@ output "jenkins_url" {
 }
 
 output "minikube_ip" {
-  value = "minikube ip"
+  value = "minikube ip komutu ile alınacak IP"
 }
 
 output "kubernetes_token" {
-  value = "${chomp(data.external.kubernetes_token.result)}"
+  value = "Dış veri kaynağından alınacak token"
 }
